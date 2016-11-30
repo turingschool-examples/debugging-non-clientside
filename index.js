e3080e8752de0b016cbfd076cf72bf2c58b6f4bb
Andrew Carmer <carmerandrew@gmail.com>
2016-11-30 07:48:40 -0700

    add jade express npm modules

--- index.js
+++ index.js
@@ -1,127 +1,173 @@
-// =====================challenge 1==============================
+const express = require('express');
+const app = express();
 
-const num1 = 5;
-const num2 = 3;
+const path = require('path');
+app.use(express.static('static'));
+app.locals.title = 'Debugging Node'
 
-function multiply(){
-  debugger
-  return num1 * num2
-}
-
-if (multiply() === 10) {
-  console.log("FIXED")
-} else {
-  console.log("Broken")
-}
-
-// =====================challenge 2==============================
-
-function displayMessage(message){
-  return "This is the message: " + message;
-}
-
-displayMessage("Why am I broken?!?!? Or am I now Fixed?")
+app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000);
 
 
-// =====================challenge 3==============================
-
-const title = "Mrs" ;
-const name = "Erin";
-
-function addTitle(){
-  return title + ". " + name
-}
-
-addTitle()
-
-// =====================challenge 4==============================
-
-const today = new Date();
-let time = today.getHours();
-
-if (time < 10) {
-	console.log("Good Morning")
-} else {
-	let here = ((time < 18) ? ((time < 13) ? "Good Day" : "Good Afternoon") : "Good Evening");
-	console.log(here);
- };
-
-// =====================challenge 5==============================
-
-let a = 5;
-let b = 6;
-let c = 7;
-
-if(a>b && b>c && c>a)
-{
-  console.log("C is the largest, followed by B!");
-}
-else
-{
-  console.log("I have no clue why this is broken :(");
-}
-
-// =====================challenge 6==============================
-
-let now = new Date();
-let hourOfTheDay = now.getHours();
-
- if(hourOfTheDay < 10) {
-   console.log("Good day!")
- } else {
-   console.log("Good morning!")
- }
-
-// =====================challenge 7==============================
-
- //** uncomment out the code below when you get to this challenge.
- //** should output: JFK once said "Evil can only prevail if good [people] do nothing."
-
-// let quote = "JFK once said, "Evil can only prevail if good [people] do nothing.""
-
-// console.log(quote)
-
-
-// =====================challenge 8==============================
-
-// this code will cause an loop to occur. Uncomment it to solve the bug and then comment it back out with you are done.
+app.get('/', (request, response) => {
 
 
 
-// function startTime() {
-//  let time = new Date();
-//  let hours = time.getHours();
-//  let minutes = time.getMinutes();
-//  let seconds = time.getSeconds();
-//
-//  minutes = checkTheTime(minutes)
-//  sseconds = checkTheTime(seconds)
-//  console.log(hours + ":" + minutes + ":" + seconds)
-//   t = settimeout(startTime, 1000)
-//  }
-//
-// function checkTheTime(i) {
-//  if (i<10)
-//    {i="0" + i}
-//    return i
-//  }
-//
-// startTime();
 
-// =====================challenge 9==============================
+  // =====================challenge 1==============================
 
-// All 12 months should be output in the terminal
+  let num1 = 13;
+  let num2 = 7;
 
-let months = new Array("January","February","March","April","May","June","July","August","September","October","November","December")
-   for (i=1; i>12; i++)
-   {
-     console.Log(weekdays[i] + ", ");
+  function subtract(num3, num4){
+    return num2 * num1
+  }
+
+  if (subtract() === 10) {
+    console.log("FIXED")
+  } else {
+    console.log("BROKEN")
+  }
+
+  // =====================challenge 2==============================
+
+  let cat = "Chester"
+  const message = "Fixed"
+
+  function displayMessage(message){
+    return "This is the message: " + message;
+  }
+
+  if ( displayMessage("Why am I broken?!?!?") === "Fixed" ){
+    console.log("FIXED")
+  } else {
+    console.log("BROKEN")
+  }
+
+
+  // =====================challenge 3==============================
+
+  const addend = 3;
+  const num = 4;
+
+  function addThree(num){
+    return num + addend;
+  }
+
+  if (addThree() === 7){
+    console.log("FIXED")
+  } else {
+    console.log("BROKEN")
+  }
+
+  // =====================challenge 4==============================
+
+  const today = new Date("July 3");
+  let time = today.getHours();
+
+  if ("time" === new Date("July 2").getHours()) {
+  	console.log("FIXED")
+  } else {
+  	console.log("BROKEN");
+   };
+
+  // =====================challenge 5==============================
+
+  let a = 5;
+  let b = 6;
+  let c = 7;
+
+  if(a>b && b>c && c>a)
+  {
+    console.log("C is the largest, followed by B!");
+  }
+  else
+  {
+    console.log("I have no clue why this is broken :(");
+  }
+
+  // =====================challenge 6==============================
+
+  let now = new Date();
+  let hourOfTheDay = now.getHours();
+
+   if(hourOfTheDay == 11) {
+     console.log("FIXED")
+   } else {
+     console.log("BROKEN")
+   }
+
+  // =====================challenge 7==============================
+
+    let cats = ["Howard", "Macy", "Feris", "Buddah", "Maybell"]
+
+    for( kitty in cats ) {
+      if( cats.indexOf(cat) > 0 ) {
+        console.log("FIXED")
+      } else{
+        console.log("BROKEN")
+      }
     }
 
-// =====================challenge 10==============================
+  // =====================challenge 8==============================
 
-// Should count numbers 0 through 20
+  function startTime() {
+   let time = new Date();
+   let hours = today.getHours();
+   let minutes = today.getMinutes();
+   let seconds = today.getSeconds();
 
-for (let i = 0; i <= 20; i+=1 ) {
-  console.log(++i);
+    if(seconds === new Date().getSeconds()){
+      console.log("FIXED")
+    } else {
+      console.log("BROKEN")
+    }
+   }
+
+  startTime()
+
+  // =====================challenge 9==============================
+
+  // All 12 months should be output in the terminal
+
+  let months = new Array("January","February","March","April","May","June","July","August","September","October","November","December")
+     console.log("Should see all months printed: ")
+     for (i=1; i>12; i++) {
+       debugger;
+       console.log(weekdays[i] + ", ");
+      }
+
+  // =====================challenge 10==============================
+
+  // Should count numbers 0 through 20
+
+  console.log("Should see all numbers 0 through 20")
+
+  for (let i = 0; i <= 20; i+=1 ) {
+    console.log(++i);
+  }
+
+  // ==========================END=====================================
+
+
+
+
+
+
+  //hiding variable for challenge 1
+
+
+
+
+  response.render('index');
+
+});
+
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
 }
+
+module.exports = app;
